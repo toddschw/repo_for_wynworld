844446c0369615a391dba5a8c6b7ee7eb68d7467
Todd Schwartzfarb <todd@wirestreak.com>
2015-12-24 12:58:51 -0500

    add cohort model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151223184730) do
+ActiveRecord::Schema.define(version: 20151224175516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cohorts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
+    t.date     "graddate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "city"
+    t.string   "state"
+    t.string   "description"
+    t.string   "website"
+    t.boolean  "hp"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -29,9 +48,15 @@ ActiveRecord::Schema.define(version: 20151223184730) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
+    t.string   "fname"
+    t.string   "lname"
+    t.integer  "cohort_id"
   end
 
+  add_index "users", ["cohort_id"], name: "index_users_on_cohort_id", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "users", "cohorts"
 end
