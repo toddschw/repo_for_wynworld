8fa3db947d4b4d1f2d5bc2e943a598ea05d422e7
Todd Schwartzfarb <todd@wirestreak.com>
2015-12-25 14:04:06 -0500

    add hash to postgresql in company table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151225173234) do
+ActiveRecord::Schema.define(version: 20151225183049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "cohorts", force: :cascade do |t|
     t.string   "name"
@@ -33,7 +34,7 @@ ActiveRecord::Schema.define(version: 20151225173234) do
     t.boolean  "hp"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.json     "orgtype"
+    t.hstore   "preferences"
   end
 
   create_table "users", force: :cascade do |t|
