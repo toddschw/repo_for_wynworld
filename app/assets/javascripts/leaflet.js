f0b956b20d494e566a6bbcbd9ddb4bfe1a9f83a5
German Grandson <ganieto1@gmail.com>
2015-12-29 15:04:45 -0500

    leaflet map

---
+++ app/assets/javascripts/leaflet.js
@@ -0,0 +1,28 @@
+/*MapBox Map*/
+  // function mapbox(){
+  //   L.mapbox.accessToken = 'pk.eyJ1IjoiZ2FuaWV0bzEiLCJhIjoiY2lpcWRleWthMDI1dXVpa3BvOGliMWJlMSJ9.SAis88xtZPzXZFBgg7UdKw';
+  //   var map = L.mapbox.map('map', 'ganieto1.oi4c63p9')
+  //     .setView([38.1264953, -97.1080468], 3);
+  //     console.log('hello')
+  // };
+
+function leaflet(){
+  var map = L.map('map').setView([38.7006009, -96.5933281], 3);
+  console.log("THIS SHIT?");
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([40.7364586, -74.0454719]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+    L.marker([25.7822569, -80.3711658]).addTo(map)
+        .bindPopup('Miami')
+        .openPopup();
+
+        L.marker([40.7364586, -74.0454719]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+
+};
