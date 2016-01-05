f81e83d472d628f914f2c83512585991f61994de
Todd Schwartzfarb <todd@wirestreak.com>
2016-01-05 11:22:02 -0500

    add several attributes to Employment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104192846) do
+ActiveRecord::Schema.define(version: 20160105162110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160104192846) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.hstore   "preferences"
+    t.string   "orgtype"
   end
 
   create_table "employments", force: :cascade do |t|
@@ -43,6 +44,8 @@ ActiveRecord::Schema.define(version: 20160104192846) do
     t.string   "jobtitle"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "roletype"
+    t.string   "rolenature"
   end
 
   add_index "employments", ["company_id"], name: "index_employments_on_company_id", using: :btree
