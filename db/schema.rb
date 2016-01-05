4784a0a89590002e59a6ba1a2b9bc38b3905e180
Todd Schwartzfarb <todd@wirestreak.com>
2016-01-05 13:31:35 -0500

    add gist indexing to status attribute (a hstore data type)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105175852) do
+ActiveRecord::Schema.define(version: 20160105182216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20160105175852) do
   add_index "users", ["cohort_id"], name: "index_users_on_cohort_id", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["status"], name: "index_users_on_status", using: :gist
 
   add_foreign_key "employments", "companies"
   add_foreign_key "employments", "users"
