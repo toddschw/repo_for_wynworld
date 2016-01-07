9cb6d62d1136ed387baafcbdd2d4bf65a84284cc
Keith Tang <keithsktang@gmail.com>
2016-01-06 23:48:40 -0500

    drop locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107020222) do
+ActiveRecord::Schema.define(version: 20160107044743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,14 +55,6 @@ ActiveRecord::Schema.define(version: 20160107020222) do
 
   add_index "employments", ["company_id"], name: "index_employments_on_company_id", using: :btree
   add_index "employments", ["user_id"], name: "index_employments_on_user_id", using: :btree
-
-  create_table "locations", force: :cascade do |t|
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
