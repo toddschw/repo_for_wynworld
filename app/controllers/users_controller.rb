485825267113d6cc50294f3c3aeb97b144011713
Todd Schwartzfarb <todd@wirestreak.com>
2016-01-18 10:03:54 -0500

    add picurl to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:fname, :lname, :email, :location)
+    params.require(:user).permit(:fname, :lname, :email, :location, :picurl)
   end
 
   def set_user
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115210247) do
+ActiveRecord::Schema.define(version: 20160118145907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,9 +53,9 @@ ActiveRecord::Schema.define(version: 20160115210247) do
   create_table "employments", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "company_id"
-    t.string   "jobtitle",   default: ""
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string   "jobtitle"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "roletype"
     t.string   "rolenature"
     t.string   "rolesource"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20160115210247) do
     t.hstore   "status"
     t.string   "name"
     t.boolean  "admin",                  default: false
+    t.string   "picurl"
   end
 
   add_index "users", ["cohort_id"], name: "index_users_on_cohort_id", using: :btree
