e55169239c0a5a02f1f84764dc34c85f642052d3
Todd Schwartzfarb <todd@wirestreak.com>
2016-01-18 09:54:04 -0500

    updated ind employment page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:fname, :lname, :email)
+    params.require(:user).permit(:fname, :lname, :email, :location)
   end
 
   def set_user
