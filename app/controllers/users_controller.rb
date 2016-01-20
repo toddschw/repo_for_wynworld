acddb85a5eccc83a67e43c73f257f2df9cefd484
Todd Schwartzfarb <todd@wirestreak.com>
2016-01-20 18:25:14 -0500

    reverse order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @employments = @user.employments.order(:start_date)
+    @employments = @user.employments.order(:start_date).reverse_order
 
   end
 
