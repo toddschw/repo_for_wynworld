6c12b7ea5b871069f0ca7523130e5d3f1834079b
Keith Tang <keithsktang@gmail.com>
2016-01-23 12:07:18 -0500

    correct note type

--- db/migrate/20160123165338_add_note_to_comapnies.rb
+++ db/migrate/20160123165338_add_note_to_comapnies.rb
@@ -2,7 +2,7 @@ class AddNoteToComapnies < ActiveRecord::Migration
   def change
     add_column :companies, :contact_first, :string
     add_column :companies, :contact_last, :string
-    add_column :companies, :note, :note
+    add_column :companies, :note, :text
     add_column :companies, :contact_email, :string
     add_column :companies, :tech_contact_f, :string
     add_column :companies, :tech_contact_l, :string
