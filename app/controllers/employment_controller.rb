4bb8af63707c690f2a4db8f010c4cbfe8e2e0258
Todd Schwartzfarb <todd@wirestreak.com>
2016-01-27 14:08:39 -0500

    latest updates

--- app/controllers/employment_controller.rb
+++ app/controllers/employment_controller.rb
@@ -10,7 +10,18 @@ class EmploymentController < ApplicationController
   end
 
   def create
-    render json: params.to_json
+    #render json: employment_params.to_json
+    #render json: params.to_json
+     @employment = Employment.new(employment_params)
+
+    #render plain: @employment.inspect
+
+    if @employment.save
+      render plain: @employment.inspect
+    else
+      render plain: "Error"
+    end
+
   end
 
   def edit
@@ -27,4 +38,8 @@ private
 
 def admin_user
   redirect_to root_url unless current_user.admin
+end
+
+def employment_params
+  params.require(:last_employment).permit(:jobtitle, :roletype, :rolenature, :rolesource, :salary, :start_date, :end_date, :current, :company_id, :user_id)
 end
