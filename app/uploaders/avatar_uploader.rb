4bea88d8295403e74a13062b660f437504df75ee
Keith Tang <keithsktang@gmail.com>
2016-01-26 23:02:42 -0500

    minimagick working

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,7 +4,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -17,7 +17,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
+  def default_url
+    [thumb, "WdXNyfs.png"].compact.join('_')
+  end
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
@@ -32,10 +34,17 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  process resize_to_fit: [480, 480]
 
+  version :profile_pic do
+    process :resize_to_fit => [250, 250]
+  end
+  version :thumb, from_version: :profile_pic do
+    process :resize_to_fit => [50, 50]
+  end
+  version :sm_thumb, from_version: :profile_pic do
+    process :resize_to_fit => [36, 36]
+  end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
