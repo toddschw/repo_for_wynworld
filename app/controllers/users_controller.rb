30df41c5da7e7c4159600bef946bb8e29b241960
Todd Schwartzfarb <todd@wirestreak.com>
2016-02-03 16:13:05 -0500

    sort feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,11 @@ class UsersController < ApplicationController
 
   def index
     # @users = User.paginate(page: params[:page])
-    @users = User.where(admin: false).includes(:companies).search(params[:keyword]).filter(params[:filter]).paginate(page: params[:page])
-  
+
+    sort_by = params[:sort_by]
+
+    @users = User.where(admin: false).order(sort_by).includes(:companies).search(params[:keyword]).filter(params[:filter]).paginate(page: params[:page])
+
 
   end
 
