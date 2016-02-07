5ec6ffcacd37e7382ebf81724e927c7bf18be43a
Todd Schwartzfarb <todd@wirestreak.com>
2016-02-07 17:11:43 -0500

    continuing work on map

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,10 +9,34 @@ class DashboardController < ApplicationController
 
     @users =  User.where(admin: false)
 
-    #@map_data = #["Google","somedude",37.3860517,-122.0838511],
+    @map_data = [ ["Google","somedude",37.3860517,-122.0838511],["Google","somedude",37.3860517,-122.0838511] ]
 
-    # companies with 1 wyncoder
-    @employments = Employment.where(current: true)
+    # Code below is used for map
+    @employments = Employment.where(current: true)  # Find all current employments
+
+    @array_with_one = []
+    @array_with_multiple = []
+
+    @employments.each do |e|
+      if @array_with_one.include? e.company.id
+        if !@array_with_multiple.include? e.company.id
+           @array_with_multiple << e.company.id
+         end
+      else
+        @array_with_one << e.company.id
+      end
+    end
+
+    @array_with_one = @array_with_one - @array_with_multiple
+
+    @general_array = []
+    @array_with_multiple.each do | multi_employer |
+      Employment.where(company_id: multi_employer).where(current: true).each do |x|
+        @general_array << x.user.fname << x.company.name
+      end
+    end
+
+
 
 
 
